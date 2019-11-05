78b9500b6615445208d59e3e0957ea9fa23123d1
nishadrin <nishadrin@gmail.com>
2019-11-05 13:05:03 +0300

    fix mistake with regex_list, use str.split instead re.split remove one variable

--- lesson_2/homework_1.py
+++ lesson_2/homework_1.py
@@ -13,8 +13,8 @@ def search_info_in_file(regex_list, file_read):
     output_list = []
     for regex in regex_list:
         re_search = re.search(f'{regex}:\ +.*', file_read)
-        re_split = re.split(':', re_search.group())
-        output_list.append(re_split[1].lstrip())
+        str_split = re_search.group().split(':')
+        output_list.append(str_split[1].lstrip())
     return output_list
 
 def get_data(files_list, regex_list):
@@ -25,8 +25,7 @@ def get_data(files_list, regex_list):
     for file in files_list:
         with open(file, 'r') as f:
             file_read = f.read()
-        search_info = search_info_in_file(regex_list, file_read)
-        info.append(search_info)
+        info.append(search_info_in_file(regex_list, file_read))
     return (main_data, *info)
 
 def write_to_csv(csv_path, files_list, regex_list):
@@ -39,10 +38,10 @@ def write_to_csv(csv_path, files_list, regex_list):
 def main():
     csv_path = os.path.abspath('homework1.csv')
     files_list = ['info_1.txt', 'info_2.txt', 'info_3.txt']
-    regex_list = (
+    regex_list = [
         'Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы',
         'Язык системы', 'Язык ввода'
-        )
+        ]
     write_csv = write_to_csv(csv_path, files_list, regex_list)
 
 if __name__ == '__main__':
