915d43dd88710ccdf68690f7c34e0f82ea878f20
nishadrin <nishadrin@gmail.com>
2019-11-08 15:18:25 +0300

    send presence msg and get answer from server

---
+++ lesson_3/client.py
@@ -0,0 +1,39 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from datetime import datetime
+import json
+
+import click
+
+
+def presence_msg():
+    return json.dumps({
+        "action": "presence",
+        "time": int((datetime.now() - datetime(1970, 1, 1)).total_seconds()),
+        "type": "status"
+        }).encode('ascii')
+
+def send_msg_to_server(socket, data):
+    return socket.send(data)
+
+def receive_msg_from_server(socket):
+    return socket.recv(1400).decode('ascii')
+
+def make_msg_from_server(data):
+    return data.decode('ascii')
+
+
+@click.command()
+@click.option('--port', default=7777, help='port number')
+@click.argument('addr')
+def command_line(addr, port):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((addr, port))
+
+    send_msg_to_server(sock, presence_msg())
+    
+    print('Сообщение с сервера:', receive_msg_from_server(sock))
+
+    sock.close
+
+if __name__ == '__main__':
+    command_line()
---
+++ lesson_3/server.py
@@ -0,0 +1,45 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import time
+import json
+
+import click
+
+
+def receive_msg_from_client(client):
+    return client.recv(1400).decode('ascii')
+
+def form_msg_to_client(data):
+    return json.dumps(data).encode('ascii')
+
+def send_msg_to_client(client, data):
+    return client.send(data)
+
+
+@click.command()
+@click.option('--port', default=7777, help='port number')
+@click.argument('addr')
+def command_line(addr, port):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((addr, port))
+    sock.listen(5)
+
+    while True:
+        print('*' * 50)
+
+        try:
+            client, addr = sock.accept()
+        except KeyboardInterrupt:
+            sock.close()
+            time.sleep(60)
+            print('Порт свободен, можно пользоваться.')
+            raise
+
+        print('Сообщение от клиента:', receive_msg_from_client(client))
+
+        send_msg_to_client(client, form_msg_to_client({"response": 200}))
+
+        client.close()
+
+
+if __name__ == '__main__':
+    command_line()
