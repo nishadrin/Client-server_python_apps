44c4008a2715b48ad174f3e5ccbd01b871aa24c5
nishadrin <nishadrin@gmail.com>
2019-11-11 09:32:25 +0300

    reload

--- lesson_3/client.py
+++ lesson_3/client.py
@@ -3,35 +3,86 @@ from datetime import datetime
 import json
 
 import click
+from client_common.settings import *
 
 
-def presence_msg():
-    return json.dumps({
+def form_data_msg(msg_to:str, msg_from:str, msg:str, encoding="ascii") -> dict:
+    return {
+        "action": "msg",
+        "time": int(datetime.now().timestamp()),
+        "to": msg_to,
+        "from": msg_from,
+        "encoding": encoding,
+        "message": msg
+        }
+
+def form_data_presence_msg(user_name:str, type:str='Status') -> dict:
+    return {
         "action": "presence",
         "time": int(datetime.now().timestamp()),
-        "type": "status"
-        }).encode('ascii')
+        "type": type,
+        "user": {
+            "account_name": user_name,
+            "status": "Hello world!"
+            }
+        }
 
-def send_msg_to_server(socket, data):
-    return socket.send(data)
+def form_data_auth(user_name:str, password:str, type:str='Status') -> dict:
+    return {
+        "action": "authenticate",
+        "time": int(datetime.now().timestamp()),
+        "type": type,
+        "user": {
+            "account_name": user_name,
+            "password": password
+            }
+        }
 
-def receive_msg_from_server(socket):
-    return socket.recv(1400).decode('ascii')
+def form_data_join_or_leave_chat(room_name, leave:bool=False) -> dict:
+    action = 'join'
+    if leave:
+        action = 'leave'
+    return {
+        "action": action,
+        "time": int(datetime.now().timestamp()),
+        "room": room_name
+        }
 
-def make_msg_from_server(data):
-    return data.decode('ascii')
+
+def send_data_to_server(connect:socket, data:dict, encoding:str='ascii'):
+    connect.send(pack_data(data, encoding))
+
+def get_data_from_server(connect:socket, encoding:str='ascii') -> dict:
+    print(connect.recv(JIM_MAX_BYTES).decode(encoding)) # b''
+    return unpack_data(connect.recv(JIM_MAX_BYTES), encoding)
+
+def pack_data(data:dict, encoding:str='ascii') -> bytes:
+    return json.dumps(data).encode(encoding)
+
+def unpack_data(data:dict, encoding:str) -> dict:
+    return json.loads(data.decode(encoding))
+
+
+def read_msg_from_server(connect:socket, data:dict) -> dict:
+    if data.get('action') == 'probe':
+        send_data_to_server(connect, form_data_presence_msg('Nick'))
+        return None
+    return data
 
 
 @click.command()
 @click.option('--port', type=int, help='port number')
 @click.argument('addr')
-def command_line(addr, port):
+def command_line(addr:str, port:int):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.connect((addr, port))
 
-    send_msg_to_server(sock, presence_msg())
+    send_data_to_server(sock, form_data_presence_msg('Nick'))
 
-    print('Сообщение с сервера:', receive_msg_from_server(sock))
+    msg_from_server = read_msg_from_server(sock, get_data_from_server(sock))
+
+    if msg_from_server is not None:
+        print('Сообщение с сервера:', msg_from_server)
 
     sock.close()
 
--- lesson_3/server.py
+++ lesson_3/server.py
@@ -4,28 +4,65 @@ import json
 
 import click
 
+from server_common.settings import *
 
-def receive_msg_from_client(client):
-    return client.recv(1400).decode('ascii')
 
-def form_msg_to_client(data):
-    return json.dumps(data).encode('ascii')
+def alerts_msg_text_from_code(response_code:int) -> str:
+    for code, msg in ALERTS_MSGS.items():
+        if int(code) == response_code:
+            return msg
+    return None
 
-def send_msg_to_client(client, data):
-    return client.send(data)
+
+def form_alert(response_code:int, msg_code:str=None) -> dict:
+    alert = 'alert'
+    if response_code > 299:
+        alert = 'error'
+    if msg_code is None:
+        msg_code = alerts_msg_text_from_code(response_code)
+    return {
+        "response": response_code,
+        "time": int(datetime.now().timestamp()),
+        'alert': msg_code
+        }
+
+def form_is_online_request() -> dict:
+    return {
+        "action": "probe",
+        "time": int(datetime.now().timestamp()),
+        }
+
+
+def unpack_data(data:dict, encoding:str) -> dict:
+    return json.loads(data.decode(encoding))
+
+def pack_data(data:dict, encoding:str) -> bytes:
+    return json.dumps(data).encode(encoding)
+
+def get_data_from_client(client:socket, encoding:str='ascii'):
+    return unpack_data(client.recv(JIM_MAX_BYTES), encoding)
+
+def send_data_to_client(client:socket, data:dict, encoding:str='ascii'):
+    client.send(pack_data(data, encoding))
+
+
+def read_msg_from_client(client):
+    client_request = get_data_from_client(client)
+    if client_request.get('action') not in ACTIONS_TUPLE:
+        return form_alert(400)
+
+    # send_data_to_client(client, form_is_online_request())
 
 
 @click.command()
-@click.option('--port', default=7777, help='port number')
-@click.option('--addr', default='0.0.0.0', help='ip address')
-def command_line(addr, port):
+@click.option('--port', default=DEFAULT_SERVER_PORT, help='port number')
+@click.option('--addr', default=DEFAULT_SERVER_IP_ADDRESS, help='ip address')
+def command_line(addr:str, port:int):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind((addr, port))
-    sock.listen(5)
+    sock.listen(SOCKET_LISTENING)
 
     while True:
-        print('*' * 50)
-
         try:
             client, addr = sock.accept()
         except KeyboardInterrupt:
@@ -34,12 +71,9 @@ def command_line(addr, port):
             print('Порт свободен, можно пользоваться.')
             raise
 
-        print('Сообщение от клиента:', receive_msg_from_client(client))
-
-        send_msg_to_client(client, form_msg_to_client({"response": 200}))
+        read_msg_from_client(client)
 
         client.close()
-
 
 if __name__ == '__main__':
     command_line()
