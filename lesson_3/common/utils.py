44e69dfcffda097976174e43d533178d60601b20
nishadrin <nishadrin@gmail.com>
2019-11-18 05:33:03 +0300

    add docstring to classes and functions in utils

--- lesson_3/common/utils.py
+++ lesson_3/common/utils.py
@@ -2,10 +2,11 @@ import json
 import socket
 from datetime import datetime
 
-from config import JIM_MAX_BYTES, ALERTS_MSGS
+from .config import JIM_MAX_BYTES, ALERTS_MSGS
 
 
 class DataPacking:
+    """ Pack/unpack data to json and encode/decode """
     def pack_data(data: dict, encoding: str) -> bytes:
         return json.dumps(data).encode(encoding)
 
@@ -14,18 +15,32 @@ class DataPacking:
 
 
 class DataExchange:
+    """ Get/send, pack/unpack and encode/decode data """
     def get_data(sock: socket, encoding: str='ascii') -> dict:
+        """ Get, decode and unpack data.
+        return dict.
+
+        """
         recieve_bytes: bytes = sock.recv(JIM_MAX_BYTES)
         if not recieve_bytes:
             return
         return DataPacking.unpack_data(recieve_bytes, encoding)
 
     def send_data(sock: socket, data: dict, encoding: str='ascii'):
+        """ Pack, encode and send data """
         sock.send(DataPacking.pack_data(data, encoding))
 
 
 class FormAlertOrError:
+    """ Create json format alert/error.
+    Have some texts for codes, see ALERTS_MSGS in configure.
+
+    """
     def alerts_msg_text_from_code(self, response_code: int) -> str:
+        """ Take text from number of error code.
+        Take text from ALERTS_MSGS in configure.
+
+        """
         for code, msg in ALERTS_MSGS.items():
             if int(code) == response_code:
                 return msg
@@ -33,6 +48,7 @@ class FormAlertOrError:
 
     def form_alert_or_error(self, response_code: int,
                             text_msg_code: str = None) -> dict:
+        """ Forming dict with alert/error """
         alert: str = 'alert'
         if response_code > 399:
             alert = 'error'
